f882274493299ef665912eba57796b065bed313c
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-11-05 21:50:18 -0700

    fix wallet hash variable

--- message.js
+++ message.js
@@ -4,9 +4,9 @@ const Client = xmtp.Client;
 
 class Message{
 
-    async send(message,wallet){
+    async send(message,walletHash){
         const client = await Client.create(wallet);
-        const conv = await client.conversations.newConversation(wallet);
+        const conv = await client.conversations.newConversation(walletHash);
         let result = await conv.send(message);
         console.log({result});
     }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const message = require('../message');
+const Message = require('../message');
+const message = new Message();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  message.send(req.body.message,req.body.wallet);
+router.get('/action', function(req, res, next) {
+  console.log(req.query.message);
+  console.log(req.query.wallet);
+  message.send(req.query.message,req.query.wallet);
 });
 
+/*
 router.post('/', function(req,res, next){
   message.send(req.body);
 });
+*/
 
 module.exports = router;
