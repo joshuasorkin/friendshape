7e3174c37d2df7334bb53268ff3f5f5734d1dcbf
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-11-05 22:05:13 -0700

    refactor client initialize to constructor

--- message.js
+++ message.js
@@ -4,11 +4,19 @@ const Client = xmtp.Client;
 
 class Message{
 
+    constructor(){
+        this.client = initializeClient();
+    }
+
+    async initializeClient(){
+        return await Client.create(wallet);
+    }
+
     async send(message,walletHash){
-        const client = await Client.create(wallet);
         const conv = await client.conversations.newConversation(walletHash);
         let result = await conv.send(message);
         console.log({result});
+        console.log(result.senderAddress);
     }
 
 }
