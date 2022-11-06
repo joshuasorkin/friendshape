e9e6f355e0963f14a02a5be19e62d938ed6cbe2d
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-11-05 21:35:34 -0700

    change message.js to class

--- message.js
+++ message.js
@@ -2,9 +2,15 @@ const xmtp = require('@xmtp/xmtp-js');
 const wallet = require('ethers').Wallet.createRandom();
 const Client = xmtp.Client;
 
-export async function send(message,wallet){
-    const client = await Client.create(wallet);
-    const conv = await client.conversations.newConversation(wallet);
-    let result = await conv.send(message);
-    console.log({result});
+class Message{
+
+    async send(message,wallet){
+        const client = await Client.create(wallet);
+        const conv = await client.conversations.newConversation(wallet);
+        let result = await conv.send(message);
+        console.log({result});
+    }
+
 }
+
+module.exports = Message;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const message = require('../message');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  message.send(req.body);
+  message.send(req.body.message,req.body.wallet);
 });
 
 router.post('/', function(req,res, next){
