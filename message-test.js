f489837b84c56d128da3aa7328fb433384691a56
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-11-06 01:10:44 -0800

    bunch of xmtp config

--- message-test.js
+++ message-test.js
@@ -1,14 +1,21 @@
-const Message = require('./message');
-const message = new Message();
-const tests = [
-    "abc   232490i",
-    "    ",
-    "abc    ",
-    "swdklfsl",
-    "   sdfsdfa",
-    "     slknkasdf aldkfgjal"
-]
-tests.forEach(test =>{
-    const result = message.parse(test);
-    console.log({result});
-});
+async function messageTest(){
+    const Message = require('./message');
+    const message = new Message();
+    const tests = [
+        "abc   232490i",
+        "    ",
+        "abc    ",
+        "swdklfsl",
+        "   sdfsdfa",
+        "     0x536a83F5b6d824e603A7E010FbFe78A424f62DaB aldkfgjal",
+        "  lkasd;jf lkkl; sklsdf",
+        "",
+        "   aksldf; lkjiosjd   "
+    ]
+    tests.forEach(async test=>{
+        const result = await message.parse(test);
+        console.log({test});
+        console.log({result});
+    });
+}
+messageTest();
--- message.js
+++ message.js
@@ -1,7 +1,11 @@
 const xmtp = require('@xmtp/xmtp-js');
-const wallet = require('ethers').Wallet.createRandom();
+require('dotenv').config();
+const {Wallet} = require('ethers');
+const wallet = new Wallet(process.env.PRIVATE_KEY);
+console.log(wallet.privateKey);
 const Client = xmtp.Client;
-const upToFirstSpace = /([^\s]+)/g;
+const client = Client.create(wallet);
+const upToFirstSpace = /([^\s]+)/;
 
 class Message{
 
@@ -10,11 +14,10 @@ class Message{
     }
 
     async initializeClient(){
-        //return await Client.create(wallet);
+        return await Client.create(wallet);
     }
 
     async send(message,walletHash){
-        const client = await Client.create(wallet);
         const conv = await client.conversations.newConversation(walletHash);
         let result = await conv.send(message);
         console.log({result});
@@ -24,17 +27,27 @@ class Message{
         console.log(process.env.TEST_WALLET);
         const client = await Client.create(wallet);
         console.log({client});
-        const conversation = await client.conversations.newConversation(process.env.TEST_WALLET);
-        for await (const message of await conversation.streamMessages()){
+        const conversation = await client.conversations.stream();
+        for await (const message of stream){
             if(message.senderAddress === client.address){
                 console.log('i sent myself a message');
                 continue;
             }
             console.log(`new message from ${message.senderAddress}: ${message.content}`);
+            let parseResult = await this.parse(message.content);
+            if (parseResult.error){
+                let errorConversation=await client.conversations.newConversation(message.senderAddress);
+                let result = await errorConversation.send(parseResult.errorMessage);
+            }
+            else{
+                console.log(`sending message to ${parseResult.walletHash}: ${parseResult.figureName}`)
+                let relayConversation=await client.conversations.newConversation(parseResult.walletHash);
+                let result = await relayConversation.send(parseResult.figureName);
+            }
         }
     }
 
-    parse(content){
+    async parse(content){
         let contentTrim = content.trim();
         if (contentTrim.length === 0){
             return {
@@ -51,7 +64,14 @@ class Message{
         }
         else{
             let walletHash = result[0];
-            let figureName = content.slice(walletHash.length).trim();
+            let figureName = contentTrim.slice(walletHash.length).trim();
+            let onDevNetwork = await this.isOnDevNetwork(walletHash);
+            if(!onDevNetwork){
+                return {
+                    error:true,
+                    errorMessage:`${walletHash} not found on XMTP dev network`
+                }
+            }
             if (figureName.length===0){
                 return {
                     error:true,
@@ -64,6 +84,12 @@ class Message{
                 figureName:figureName
             }
         }
+
+        
+    }
+    async isOnDevNetwork(walletHash){
+            let canMessage = await Client.canMessage(walletHash);
+            return canMessage;
     }
 }
 
